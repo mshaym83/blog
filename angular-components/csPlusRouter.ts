7c599d70cd8b42adad7d878b0d4771f7f209bbe4
Shyam Mohan <mshyam83@gmail.com>
2018-06-12 21:51:35 +0530

    Revert "added angular libs"
    This reverts commit 61850a80b99acbee1904dc1f7db9bcad4d1161a1.

--- angular-components/csPlusRouter.ts
+++
@@ -1,6 +0,0 @@
-import {Routes} from "@angular/router";
-import { UserLoginComponent } from "./user/login/user.login.component";
-export const csPlusRouter:Routes = [
-    {path:'login',component:UserLoginComponent},
-    {path:'', component: UserLoginComponent}
-];
--- angular-components/nav/nav.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-    templateUrl: './nav.component.html',
-    selector: 'nav-bar'
-})
-export class NavbarComponent {
-    
-}
